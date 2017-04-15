cac66dcdd2cef547eedbe6d61c9665ed735fe603
pietroglyph <declanfreemangleason@gmail.com>
2017-04-15 02:16:45 -0700

    Added basic scaffholding.

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+type configuration struct {
+	Host        string
+	Port        string
+	EnableTLS   bool
+	TLSCertPath string
+	TLSKeyPath  string
+	APIKey      string
+}
+
+type reqError struct {
+	Error   error
+	Message string
+	Code    int
+}
+
+type gameList struct {
+	games map[int]string
+	mux sync.Mutex
+}
+
+type reqHandler func(http.ResponseWriter, *http.Request) *reqError
+
+var config configuration
+var err error
+
+func init() {
+	flag.StringVar(&config.Host, "host", "localhost", "host to listen on for the webserver")
+	flag.StringVar(&config.Port, "port", "8000", "port to listen on for the webserver")
+	flag.BoolVar(&config.EnableTLS, "tls", false, "enable serving with TLS (https)")
+	flag.StringVar(&config.TLSCertPath, "tls-cert", folderPath+"/cert.pem", "path to certificate file")
+	flag.StringVar(&config.TLSKeyPath, "tls-key", folderPath+"/key.pem", "path to private key for certificate")
+	flag.StringVar(&config.APIKey, "api-key", "XXXXXXXXXXXXXXXXX", "steam api key")
+	flag.Parse() // Parse the rest of the flags
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	if !config.EnableTLS {
+		err = http.ListenAndServe(bind, nil)
+		if err != nil {
+			panic(err)
+		}
+	} else if config.EnableTLS {
+		fmt.Println("Serving with TLS...")
+		err = http.ListenAndServeTLS(bind, config.TLSCertPath, config.TLSKeyPath, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func handler(resWriter http.ResponseWriter, reqHttp *http.Request) *reqError {
+	defer func() { // Recover from a panic if one occurred
+		if err := recover(); err != nil {
+			fmt.Println(err)
+			fmt.Fprint(resWriter, err)
+		}
+	}()
+
+	if reqHttp.URL.Query().Get("players") != "" {
+		players := strings.Split(reqHttp.URL.Query().Get("players"))
+	} else {
+		return &reqError{errors.New("Invalid query parameter"), "Invalid query paramter", 400}
+	}
+
+	for i := range players {
+		go func() {
+
+		}
+	}
+}
+
+func (fn reqHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if e := fn(w, r); e != nil { // e is *appError, not os.Error
+		http.Error(w, e.Message, e.Code) // Serve an error message
+	}
+}
