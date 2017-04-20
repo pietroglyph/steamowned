f038509fbe0735bf896d02caca54e03202afb1bc
pietroglyph <declanfreemangleason@gmail.com>
2017-04-19 20:24:51 -0700

    Now works. Correctly parses XML and returns game ids, human readable names aren't implemented.

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
+	"github.com/bjarneh/bloomfilter"
+	"github.com/jteeuwen/go-pkg-xmlx"
 	"log"
 	"net/http"
 	"strings"
-	"encoding/json"
 	"sync"
 )
 
@@ -33,27 +35,29 @@ type gameList struct {
 type reqHandler func(http.ResponseWriter, *http.Request) *reqError
 
 var config configuration
+var wg sync.WaitGroup // For determining when we're done fetching and eliminating all of the games a user owns
 var err error
 
 func init() {
 	flag.StringVar(&config.Host, "host", "localhost", "host to listen on for the webserver")
 	flag.StringVar(&config.Port, "port", "8000", "port to listen on for the webserver")
 	flag.BoolVar(&config.EnableTLS, "tls", false, "enable serving with TLS (https)")
-	flag.StringVar(&config.TLSCertPath, "tls-cert", folderPath+"/cert.pem", "path to certificate file")
-	flag.StringVar(&config.TLSKeyPath, "tls-key", folderPath+"/key.pem", "path to private key for certificate")
-	flag.StringVar(&config.APIKey, "api-key", "XXXXXXXXXXXXXXXXX", "steam api key")
-	flag.Parse() // Parse the rest of the flags
+	flag.StringVar(&config.TLSCertPath, "tls-cert", "/cert.pem", "path to certificate file")
+	flag.StringVar(&config.TLSKeyPath, "tls-key", "/key.pem", "path to private key for certificate")
+	flag.StringVar(&config.APIKey, "api-key", "XXXXXXXXXXXXXXXXX", "steam api key") // If not specified the API will return a 403 (XXXXXXXXXXXXXXXXX is not a valid API key)
+	flag.Parse()                                                                    // Parse the rest of the flags
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.Handle("/", reqHandler(handler))
+	bind := fmt.Sprintf("%s:%s", config.Host, config.Port)
 	if !config.EnableTLS {
 		err = http.ListenAndServe(bind, nil)
 		if err != nil {
 			panic(err)
 		}
 	} else if config.EnableTLS {
-		fmt.Println("Serving with TLS...")
+		log.Println("Serving with TLS...")
 		err = http.ListenAndServeTLS(bind, config.TLSCertPath, config.TLSKeyPath, nil)
 		if err != nil {
 			panic(err)
@@ -69,22 +73,54 @@ func handler(resWriter http.ResponseWriter, reqHttp *http.Request) *reqError {
 		}
 	}()
 
+	var players []string // For storing the split query parameter
 	if reqHttp.URL.Query().Get("players") != "" {
-		players := strings.Split(reqHttp.URL.Query().Get("players"))
+		players = strings.Split(reqHttp.URL.Query().Get("players"), "|")
 	} else {
 		return &reqError{errors.New("Invalid query parameter"), "Invalid query paramter", 400}
 	}
 
+	var games gameList                 // Holds the mutex and map of games
+	games.games = make(map[int]string) // Initalize the map
 	for i := range players {
-		go func(steamId string) {
-			resp, err := http.Get("https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + config.APIKey + "&steamid=" + steamId + "&format=json")
-			if err != nil {
-				log.Println(err.Error())
+		wg.Add(1) // Add for each goroutine made
+		go func(steamId string, glist gameList) {
+			defer wg.Done() // Make sure we always say we're done when we are
+
+			doc := xmlx.New()                                                                                                                         // Make a new document to store xml data in
+			doc.SetUserAgent("steamownedbot/1.0 (+https://github.com/pietroglyph/steamowned)")                                                        // Be polite
+			doc.LoadUri("https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="+config.APIKey+"&steamid="+steamId+"&format=xml", nil) // Get xml data from the Steam API
+			gamenodes := doc.SelectNodes("", "appid")                                                                                                 // gamenodes is *all* nodes with appid as their key
+
+			if len(gamenodes) == 0 { // Probably an invalid SteamID64 parameter
+				log.Println("Couldn't extract appids using SteamID", steamId)
 				return
 			}
-			json.
-		}(players[i])
+
+			glist.mux.Lock() // We're going to start using the map of games
+			defer glist.mux.Unlock()
+			if len(glist.games) == 0 {
+				for i := range gamenodes {
+					glist.games[gamenodes[i].I("", "appid")] = gamenodes[i].S("", "appid")
+				}
+			} else {
+				filter := bloomfilter.New() // This is because gamenodes is not indexed by game-name, the bloom filter might be slower, but I wanted to use it
+				for i := range gamenodes {
+					filter.Add(gamenodes[i].S("", "appid"))
+				}
+				for k := range glist.games {
+					if !filter.Marked(glist.games[k]) { // Game games[k] is not owned (does not exist) in gamenodes, so we delete it
+						delete(glist.games, k)
+					}
+				}
+			}
+		}(players[i], games)
 	}
+	wg.Wait()
+	for i := range games.games {
+		fmt.Fprint(resWriter, games.games[i], " ")
+	}
+	return nil // Nothing went wrong
 }
 
 func (fn reqHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
